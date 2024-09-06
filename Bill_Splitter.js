e57729e539a54892c5c623454239a3273e0bbdeb
Martin Leone <martinnikolaslll@gmail.com>
2024-09-06 13:52:14 -0400

    Create and test calculateTip function

--- Bill_Splitter.js
+++ Bill_Splitter.js
@@ -17,7 +17,7 @@ console.log('The bill was $${bill}, the tip was $${tip.toFixed(2)}, and the tota
 
 //Task 3: Create calculateTip function
 
-const calculateTip = (tip);
+const calculateTip = (bill) => (bill >= 50 && bill <= 300) ? bill * 0.15 : bill * 0.20;
 //Testing function
 let testBill = 100;
 let testTip = calculateTip(testBill);
